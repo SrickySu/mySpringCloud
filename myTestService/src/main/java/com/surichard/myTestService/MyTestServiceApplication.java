08db1f5b5a910b03c918d0b04477bb152c90dda8
74518 <74518@192.168.1.100>
2018-12-23 17:59:22 +0800

    add eureka

--- myTestService/src/main/java/com/surichard/myTestService/MyTestServiceApplication.java
+++ myTestService/src/main/java/com/surichard/myTestService/MyTestServiceApplication.java
@@ -2,8 +2,10 @@ package com.surichard.myTestService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class MyTestServiceApplication {
 
 	public static void main(String[] args) {
