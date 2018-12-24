0f80ae1bd9624a3916fc7a5f0294a8033ffd0d6a
su.qingjia <su.qingjia@yuexiu-finance.com>
2018-12-24 16:42:21 +0800

    add spring cloud config component

--- myEurekaServer/src/main/java/com/surichard/myEurekaServer/MyEurekaServerApplication.java
+++ myEurekaServer/src/main/java/com/surichard/myEurekaServer/MyEurekaServerApplication.java
@@ -2,10 +2,12 @@ package com.surichard.myEurekaServer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
+@EnableConfigServer
 public class MyEurekaServerApplication {
 
 	public static void main(String[] args) {
