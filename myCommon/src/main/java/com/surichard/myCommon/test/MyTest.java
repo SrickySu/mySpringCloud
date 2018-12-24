a6140c12caa53d81f4fee5bc69942e0936c8eda2
su.qingjia <su.qingjia@yuexiu-finance.com>
2018-12-24 11:16:03 +0800

    整理文件结构

---
+++ myCommon/src/main/java/com/surichard/myCommon/test/MyTest.java
@@ -0,0 +1,13 @@
+package com.surichard.myCommon.test;
+
+import java.time.LocalDate;
+
+public class MyTest {
+
+	public static void main(String[] args) {
+		LocalDate now = LocalDate.now();
+		System.out.println(now);
+
+	}
+
+}
