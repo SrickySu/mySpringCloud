61ce16d2d54ee6df7e0aff6669a0d6cb30d78332
su.qingjia <su.qingjia@yuexiu-finance.com>
2019-01-25 17:16:05 +0800

    update

--- myService1/src/main/java/com/surichard/myService1/controller/MyService1Controller.java
+++ myService1/src/main/java/com/surichard/myService1/controller/MyService1Controller.java
@@ -3,6 +3,7 @@ package com.surichard.myService1.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,17 +13,21 @@ import com.surichard.myService1.service.TimeZoneService;
 
 @RestController
 public class MyService1Controller {
-	
+
 	private static final Logger logger = LoggerFactory.getLogger(MyService1Controller.class);
-	
+
 	@Autowired
 	private TimeZoneService timeZoneService;
 
+	@Value(value = "${spring.application.name}")
+	private String applicationName;
+
 	@GetMapping(value = "/information")
 	public String getInformation(@RequestParam(name = "id", required = false) Integer id) {
-		logger.info("request id is {}", id);
+		logger.info("applicationName is {}", applicationName);
 		TimeZoneEntity item = timeZoneService.getTimeZoneById(id);
-		item = item == null? new TimeZoneEntity() : item;
+		item = item == null ? new TimeZoneEntity() : item;
+		logger.info("time zone is {}", item);
 		return "Good Job! This is my service1. TimeZone " + id + " is " + item.getUseLeapSeconds();
 	}
 }
